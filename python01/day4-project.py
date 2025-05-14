6e30c9e2b0960f2de4e0c0e37e9c082b984a6730
imoranos <imoranous@gmail.com>
2025-05-15 00:43:36 +0100

    project 01 in python

--- python01/day4-project.py
+++ python01/day4-project.py
@@ -1,27 +1,59 @@
 def arithmetic_arranger(problems, show_answers=False):
-    numberL = []
-    
+    number1 = []
+    operater = []
+    number2 = []
     for i in problems:
+        if i.find('-') >= 1:
+            index_oper = i.find('-')
+            oper = i[index_oper]
+        elif i.find('+') >= 1:
+            index_oper = i.find('+')
+            oper = i[index_oper]
+        else:
+            print('operator not found')
+            break
+        counter = 0
+        print(index_oper)
+        print(oper)
         for k in i:
             
-            if k == '+' or k == '-':
-                opera = k 
-                break
-            else:
-                numberL.append(k)
-               
-    print(numberL)
-    print(opera)
-    numberL = []
-    numberR = []
+            if counter < index_oper and k != ' ':
+                
+                number1.append(k)
+            
+            if counter > index_oper and k != ' ':
+                number2.append(k)
+            counter +=1
+
+            #number1.append(k)
+        print(number1)
+        print(number2)
+        number1 = int(''.join(map(str, number1)))
+        number2 = int(''.join(map(str, number2)))
+        print(number1)
+        print(oper)
+        print(number2)
+        if oper == '+':
+            total = number1 + number2
+        elif oper == '-':
+            total = number1 - number2
+        print(f'total: {total}')    
+
+        #print(numberL)
+        
+        
+         
      
+      
+      
 
     return
 
 print(f'\n{arithmetic_arranger(["32 + 698"])}')
 
 # ghan9sam l3amaliya naghd ra9m lwlani nhto f varibl otani f variable o l3alama f variable 
-#
-#
-#
-#
+#first number
+#operater
+#second number
+#gad function dyal total
+#gad function likathwl list l int 
---
+++ readme.md
@@ -0,0 +1,4 @@
+# function
+def function name:
+     pass
+     map(lambda x: x * 2, [1, 2, 3]) 
