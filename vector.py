8bb3d6488f79bddf2a58369fd029cabe321b8201
imoranos <imoranous@gmail.com>
2025-06-02 15:58:04 +0100

    Learn Special Methods by Building a Vector Space

--- vector.py
+++ vector.py
@@ -26,14 +26,63 @@ class R2Vector:
         if type(self) != type(other):
             return NotImplemented
         kwargs = {i: getattr(self, i) + getattr(other, i) for i in vars(self)}
-        return kwargs    
+        return self.__class__(**kwargs)    
+    
+    def __sub__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        kwargs = {i: getattr(self, i) - getattr(other, i) for i in vars(self)}
+        return self.__class__(**kwargs)
+    def __mul__(self, other):
+        if type(other) in (int, float):
+            kwargs = {i: getattr(self, i) * other for i in vars(self)}
+            return self.__class__(**kwargs)
+        
+        elif type(self) == type(other): 
+            # args = [1,2,3,4,] -> sum(args) -> 1+2+3+4 ..
+            args = [getattr(self, i) * getattr(other, i) for i in vars(self)]
+            return sum(args)
+        return NotImplemented
+    
+    def __eq__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        return all(getattr(self, i) == getattr(other, i) for i in vars(self))
+        
+    def __ne__(self, other):
+        return not self == other
 
+    def __lt__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        return self.norm() < other.norm()
+
+    def __gt__(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        return self.norm() > other.norm()
+
+    def __le__(self, other):
+        return not self > other
+
+    def __ge__(self, other):
+        return not self < other
+        
 class R3Vector(R2Vector):
     def __init__(self, *, x, y, z):
         super().__init__(x=x, y=y)
         self.z = z
-         
 
+    def cross(self, other):
+        if type(self) != type(other):
+            return NotImplemented
+        kwargs = {
+            'x': self.y * other.z - self.z * other.y,
+            'y': self.z * other.x - self.x * other.z,
+            'z': self.x * other.y - self.y * other.x
+        }
+        
+        return self.__class__(**kwargs)    
 
 # object ---------------------------
 v1 = R2Vector(x=2, y=3)
@@ -48,6 +97,20 @@ v3 = R2Vector(x=5, y=10)
 print(f'v1 = {v1}')
 print(f'v2 = {v2}')
 print(v1.__add__(v3))
+
+#final step
+v1 = R3Vector(x=2, y=3, z=1)
+v2 = R3Vector(x=0.5, y=1.25, z=2)
+print(f'v1 = {v1}')
+print(f'v2 = {v2}')
+v3 = v1 + v2
+print(f'v1 + v2 = {v3}')
+v4 = v1 - v2
+print(f'v1 - v2 = {v4}')
+v5 = v1 * v2
+print(f'v1 * v2 = {v5}')
+v6 = v1.cross(v2)
+print(f'v1 x v2 = {v6}')
 
 # dict = {'x': 2 ,'y': 3}
 # dict["x"]
